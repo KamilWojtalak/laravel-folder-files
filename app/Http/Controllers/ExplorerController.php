97bda0636b94d2c38d455daf7a3f5ed1b8825a2f
Kamil Wojtalak <kamilwojtalak99@gmail.com>
2024-05-25 09:22:07 +0200

    explorere show view, return showFolders and showFiles

--- app/Http/Controllers/ExplorerController.php
+++ app/Http/Controllers/ExplorerController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\File;
 use App\Models\Folder;
 use Illuminate\Http\Request;
 use \Illuminate\Contracts\View;
@@ -10,6 +11,12 @@ class ExplorerController extends Controller
 {
     public function show(Folder $folder): View\View|View\Factory
     {
-        return view('folder.show', compact('folder'));
+        $showFolders = Folder::parent($folder)->get();
+        $showFiles = File::parent($folder)->get();
+
+        return view(
+            'folder.show',
+            compact('folder', 'showFolders', 'showFiles')
+        );
     }
 }
