c828ba41dfd5e92456a6e56c43555b5fb64b78b3
Kamil Wojtalak <kamilwojtalak99@gmail.com>
2024-05-25 07:47:21 +0200

    add debug info

--- app/Http/Controllers/ExplorerController.php
+++ app/Http/Controllers/ExplorerController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Folder;
 use Illuminate\Http\Request;
 
 class ExplorerController extends Controller
 {
-    public function show(?Folder $folder)
+    public function show(Folder $folder)
     {
-        dd('dfsaasdfas');
+        dd('dfsaasdfas', $folder);
         //
     }
 }
