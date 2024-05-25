fb752f8fe5c39ed1f6928d3e3f81898a49a5d309
Kamil Wojtalak <kamilwojtalak99@gmail.com>
2024-05-25 09:38:27 +0200

    add file show blade

---
+++ resources/views/file/show.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body>
+    <p>
+        <a href="{{ route('explorer.show', $file->parent_id) }}">
+            Wróć
+        </a>
+    </p>
+
+    <h3>Plik: {{ $file->name }}</h3>
+</body>
+
+</html>
