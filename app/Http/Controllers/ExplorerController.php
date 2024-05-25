c2c303ec6c29801ff654cc3675a7c0be75ede7e7
Kamil Wojtalak <kamilwojtalak99@gmail.com>
2024-05-25 07:44:09 +0200

    add explorer class

---
+++ app/Http/Controllers/ExplorerController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ExplorerController extends Controller
+{
+    public function show(?Folder $folder)
+    {
+        dd('dfsaasdfas');
+        //
+    }
+}
