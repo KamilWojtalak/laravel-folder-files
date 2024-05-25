57169409db57ca475637b7f87749f38a75034439
Kamil Wojtalak <kamilwojtalak99@gmail.com>
2024-05-25 09:21:24 +0200

    add scope folder

--- app/Models/File.php
+++ app/Models/File.php
@@ -2,12 +2,24 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Database\Query\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class File extends Model
 {
     use HasFactory;
 
     protected $guarded = [];
+
+    public function folder(): BelongsTo
+    {
+        return $this->belongsTo(Folder::class);
+    }
+
+    public function scopeFolder(Builder $q, Folder $parent): Builder
+    {
+        return $q->where('folder_id', $parent->id);
+    }
 }
