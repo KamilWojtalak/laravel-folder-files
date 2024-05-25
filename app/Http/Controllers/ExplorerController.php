fbb83db981cfb8f1f7263764150abe4353166c8d
Kamil Wojtalak <kamilwojtalak99@gmail.com>
2024-05-25 07:49:08 +0200

    explorer show methods returns view

--- app/Http/Controllers/ExplorerController.php
+++ app/Http/Controllers/ExplorerController.php
@@ -4,12 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Models\Folder;
 use Illuminate\Http\Request;
+use \Illuminate\Contracts\View;
 
 class ExplorerController extends Controller
 {
-    public function show(Folder $folder)
+    public function show(Folder $folder): View\View|View\Factory
     {
-        dd('dfsaasdfas', $folder);
-        //
+        return view('folder.show', compact($folder));
     }
 }
